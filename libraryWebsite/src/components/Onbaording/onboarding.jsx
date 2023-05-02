7da945335b543f80c07b9d308a74fbb8f887c346
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2023-05-02 16:27:20 +0100

    image added to onbaord page and layout activated

--- libraryWebsite/src/components/Onbaording/onboarding.jsx
+++ libraryWebsite/src/components/Onbaording/onboarding.jsx
@@ -1,10 +1,18 @@
-import './onboarding.css'
-
+import "./onboarding.css";
+import LandingLayout from "../Layout/landingLayout";
+import Onbaord_graphics from "/images/lib.png";
 
 export default function Onboarding() {
   return (
     <>
-      <h2>onboard</h2>
+      <LandingLayout>
+        <div className="onboard_page">
+          <section className="onboard_grahics">
+            <img src={Onbaord_graphics} alt="library image" />
+          </section>
+          <section></section>
+        </div>
+      </LandingLayout>
     </>
-  )
+  );
 }
