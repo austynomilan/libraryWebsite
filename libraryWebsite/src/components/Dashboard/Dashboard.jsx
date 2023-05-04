20be492940f0be266215f6f7006b276932e1439c
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2023-05-04 06:29:46 +0100

    top layer dashbd component implrmented

--- libraryWebsite/src/components/Dashboard/Dashboard.jsx
+++ libraryWebsite/src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,18 @@
-import './Dashboard.css'
+import "./Dashboard.css";
 import LandingLayout from "../Layout/landingLayout";
-
+import { HiOutlineSearchCircle } from "react-icons/Hi";
 
 export default function Dashboard() {
   return (
     <LandingLayout>
-      
+      <section className="feed_search">
+        <input type="search" name="search" placeholder="Search For Gazette" />
+        <HiOutlineSearchCircle className="icon" size={20} color="#fff" />
+      </section>
+<section className="dash_board_header">
+  <h2>Upload File</h2>
+</section>
+
     </LandingLayout>
-  )
+  );
 }
